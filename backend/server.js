7d5f67414989d7cdc534616e6766e7297c903f8a
Elias Araneda <emedim.araneda@gmail.com>
2024-09-12 13:12:15 +0200

    update backend file

--- backend/server.js
+++ backend/server.js
@@ -1,58 +1,40 @@
-const express = require('express'); // création de express pour initialiser les routes
+const express = require('express');
 const { Pool } = require('pg');
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 
 const pool = new Pool({
-    connectionString: 'postgres://user:password@localhost:5432/ebookdb'
+    connectionString: process.env.DATABASE_URL
 });
 
-const cors = require('cors');
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
-
-
-//TEST de la route de base
-app.get('/', (req, res) => {
-    res.send('Can i have some beer!');
-});
 
 app.get('/books', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM books');
         res.json(result.rows);
     } catch (err) {
-        console.error(err.message);
+        console.error('Erreur lors de la récupération des livres:', err.message);
         res.status(500).send('Server error');
-    }       
+    }
 });
 
 app.post('/books', async (req, res) => {
-    try{
+    try {
         const { title, author, year, isbn } = req.body;
         const newBook = await pool.query(
             'INSERT INTO books (title, author, year, isbn) VALUES ($1, $2, $3, $4) RETURNING *',
             [title, author, year, isbn]
         );
         res.json(newBook.rows[0]);
-    }   catch (err) {
-        console.log(err.message);
+    } catch (err) {
+        console.log('Erreur lors de l\'ajout du livre:', err.message);
         res.status(500).send('Server error');
     }
 });
 
 app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
-
-
-app.delete('/books/:id', async (req,res) => {
-    try {
-        const { id } = req.params;
-        await pool.query('DELETE FROM books WHERE id = $1', [id]);
-        res.json('Book deleted');
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).send('Server error');
-    }
+    console.log('Server running on port 5000');
 });
